8b45aa16747dc8811d8efd5067fa095d6a6d82b3
DanielSFRa <daniel@hugtak.com>
2025-06-03 08:55:44 -0300

    11 a 17

---
+++ 5_OP_Expressoes/16_op_de_diferenca/index.php
@@ -0,0 +1,51 @@
+<?php
+// operador de diferença
+/* com o opeadore de diferença verificamos se um valor
+é diferente de outro 
+O simbolo é : !=
+Exemplo: 5 != # false
+Exemplo: 10 != 5 # true
+*/
+
+$a = 3;
+$b = 4;
+
+
+if($a != $b){
+    echo "Testando diferença 1<br>";
+
+
+}
+
+if($a != 3){
+    echo "Testando diferença 2<br>";
+
+
+}
+
+if(false != "teste"){
+    echo "Testando diferença 3<br>";
+
+
+}
+
+if(3 != "3"){
+    echo "Testando diferença 4<br>";
+
+
+}
+if(3 != "4"){
+    echo "Testando diferença 5<br>";
+
+
+}
+
+
+
+
+
+
+
+
+
+?>
