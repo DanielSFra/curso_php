fda73b9fd966de7681ff15c1cbf38a519d3bb711
DanielSFRa <daniel@hugtak.com>
2025-06-03 17:09:53 -0300

    pasta 17 a 23

---
+++ 5_OP_Expressoes/23_op_or/index.php
@@ -0,0 +1,19 @@
+<?php
+// operado or seu símbolo é ||
+// O Operador lógico OR resulta em verdadeiro caso qualquer
+// um dos lados da operação seja verdadeiro;
+// E só resulta em falso caso os dois lados sejam falso;
+// símbolo : ||
+//exemplo ; 5 > 15 || "teste" == "teste" # true
+// apenas umas das afirmativas precisa ser verdadeiro
+
+
+
+
+
+if(5 > 15 || "teste" == "teste"){
+   echo"é verdadeiro";
+}
+
+
+?>
